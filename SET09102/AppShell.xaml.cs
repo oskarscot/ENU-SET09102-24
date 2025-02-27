ad1b17ad8292732714570d27c352b6d79ab81f50
oskarscot <oskar.budzisz4@gmail.com>
2025-02-27 13:43:31 +0000

    Init Project

---
+++ SET09102/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace SET09102;
+
+public partial class AppShell : Shell
+{
+    public AppShell()
+    {
+        InitializeComponent();
+    }
+}
---
+++ SET09102/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace SET09102;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+    protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
