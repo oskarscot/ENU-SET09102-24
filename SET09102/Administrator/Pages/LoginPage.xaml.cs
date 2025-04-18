6592d3eb835567d352a544a5a9de7b1e9b559a71
Caswell30 <40599726@live.napier.ac.uk>
2025-04-18 14:15:29 +0100

    Enhanced application structure by adding administration features: - Updated AppShell to include login and administration pages - Registered navigation routes for new pages - Implemented services for user and sensor management - Refactored LoginPage and SensorConfigPage for improved data binding and functionality - Added unit tests for UserService with mock dependencies

--- SET09102/Administrator/Pages/LoginPage.xaml.cs
+++ SET09102/Administrator/Pages/LoginPage.xaml.cs
@@ -1,39 +1,74 @@
 using Microsoft.Data.SqlClient;
 using SET09102.Services.Administration;
+using SET09102.Administrator.Services;
 using System.ComponentModel;
 using System.Runtime.CompilerServices;
 using System.Windows.Input;
 
 namespace SET09102.Administrator.Pages
 {
-    public partial class LoginPage : ContentPage, INotifyPropertyChanged
+    public partial class LoginPage : ContentPage
     {
         private readonly AuthService _authService;
-        public string Username { get; set; }
-        public string Password { get; set; }
-        public string LockoutMessage { get; set; }
+        private readonly string _connectionString;
+        private readonly AuditService _auditService;
+        private string _username;
+        private string _password;
+        private string _lockoutMessage;
+
+        public string Username 
+        { 
+            get => _username;
+            set
+            {
+                _username = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public string Password 
+        { 
+            get => _password;
+            set
+            {
+                _password = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public string LockoutMessage 
+        { 
+            get => _lockoutMessage;
+            set
+            {
+                _lockoutMessage = value;
+                OnPropertyChanged();
+                OnPropertyChanged(nameof(IsLockedOut));
+            }
+        }
+
         public bool IsLockedOut => !string.IsNullOrEmpty(LockoutMessage);
 
-        public LoginPage(AuthService authService)
+        public LoginPage(AuthService authService, string connectionString, AuditService auditService)
         {
             InitializeComponent();
             _authService = authService;
+            _connectionString = connectionString;
+            _auditService = auditService;
+            BindingContext = this;
         }
 
         public ICommand LoginCommand => new Command(async () =>
         {
             if (await _authService.LoginAsync(Username, Password))
-                await Navigation.PushAsync(new UserManagementPage(new UserService(new SqlConnection("YourConnectionString"))));
+                await Navigation.PushAsync(new UserManagementPage(new UserService(_connectionString, _auditService)));
             else
                 LockoutMessage = "Login failed. Try again or check lockout.";
-            OnPropertyChanged(nameof(LockoutMessage));
-            OnPropertyChanged(nameof(IsLockedOut));
         });
 
-        public event PropertyChangedEventHandler PropertyChanged;
-        private void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        protected override void OnPropertyChanged([CallerMemberName] string propertyName = null)
         {
-            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+            base.OnPropertyChanged(propertyName);
         }
     }
 }
--- SET09102/AppShell.xaml.cs
+++ SET09102/AppShell.xaml.cs
@@ -1,9 +1,19 @@
-﻿namespace SET09102;
+﻿using SET09102.Administrator.Pages;
+
+namespace SET09102;
 
 public partial class AppShell : Shell
 {
     public AppShell()
     {
         InitializeComponent();
+        
+        // Register routes for navigation
+        Routing.RegisterRoute(nameof(LoginPage), typeof(LoginPage));
+        Routing.RegisterRoute(nameof(UserManagementPage), typeof(UserManagementPage));
+        Routing.RegisterRoute(nameof(SensorManagementPage), typeof(SensorManagementPage));
+        Routing.RegisterRoute(nameof(SensorConfigPage), typeof(SensorConfigPage));
+        Routing.RegisterRoute(nameof(DataManagementPage), typeof(DataManagementPage));
+        Routing.RegisterRoute(nameof(AuditLogPage), typeof(AuditLogPage));
     }
 }
